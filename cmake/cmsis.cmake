13ebb9cec5115932de65955ce570822af3f0ffae
Sébastien Besombes <sebastien.besombes@gmail.com>
2020-03-25 22:37:09 +0100

    Corrected bug

--- cmake/cmsis.cmake
+++ cmake/cmsis.cmake
@@ -3,5 +3,5 @@ message("cmsis library path ${LIBRARY_PATH}")
 set(CMSIS_LIBRARY_PATH "${LIBRARY_PATH}/cmsis")
 
 include_directories(
-	${CMSIS_LIBRARY_PATH}/cmsis/CMSIS/Core/include
+	${LIBRARY_PATH}/cmsis/CMSIS/Core/Include
 )
