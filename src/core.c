c9090aec98a191c8cde357331f599940cc096640
seb <seb@seb-HP-EliteBook-840-G3>
2020-07-17 15:52:33 +0200

    Removed libc

--- src/core.c
+++ src/core.c
@@ -1,9 +1,22 @@
 #include <core.h>
 
+// see https://m0agx.eu/2018/08/14/reducing-firmware-size-by-removing-libc/
+
 int main(void)
 {
 	setup() ;
 
 	while(1)
 		loop() ;
+}
+
+__attribute__((weak)) void _start(void)
+{
+	main() ;
+}
+
+// needed by linker nano.specs
+__attribute__((weak, noreturn)) void _exit(int a __attribute__((unused)))
+{
+	__builtin_unreachable() ;
 }
